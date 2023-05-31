f3a1d33f0304bed3531f472b9e6c193ef50e7439
Brod Cheong <97454249+Brod-Cheong@users.noreply.github.com>
2023-05-31 15:58:36 +0800

    Time: 248 ms (36.27%) | Memory: 23.7 MB (31.41%) - LeetSync

--- product-of-array-except-self/product-of-array-except-self.py
+++ product-of-array-except-self/product-of-array-except-self.py
@@ -1,17 +1,18 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        size = len(nums)
-        leftProduct = [0]*size
-        rightProduct = [0]*size
-        leftProduct[0] = 1
-        rightProduct[size-1] = 1
-        
-        for i in range(1,size):
-            leftProduct[i] = leftProduct[i-1] * nums[i-1]
-            rightProduct[size-i-1] = rightProduct[size-i] * nums[size-i]
-        
-        
-        ans = []
-        for i in range(size):
-            ans.append(leftProduct[i] * rightProduct[i])
-        return ans
+        res = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+        # "len(nums) - 1" is the starting point of the sequence. It represents the index of the last element in the list or iterable object "nums". The "-1" is used because indexing in Python starts from 0, so this expression gets the index of the last element.
+
+        # "-1" is the stop value of the sequence. It specifies the value at which the sequence will stop generating numbers. In this case, it stops at -1, inclusive.
+
+        # "-1" is the step value, which determines the increment or decrement between consecutive numbers in the sequence. In this case, it is set to -1, indicating that the numbers will be generated in descending order, decrementing by 1 each time.
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
