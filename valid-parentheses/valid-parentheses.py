b3a5c92041d560634e22082c67191799078e3e9f
Brod Cheong <97454249+Brod-Cheong@users.noreply.github.com>
2023-06-05 18:18:32 +0800

    Update valid-parentheses.py

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -7,16 +7,6 @@ class Solution:
 
         }
         
-        #this only works for "{}(){}" not "[()]"
-        # for idx, i in enumerate(s):
-        #     if i in brac_dict.keys(): #if is a opening bracket
-        #         if  brac_dict[i]== s[idx+1]: #check for closing bracket
-        #             continue
-        #         else:
-        #             return False
-        #     else:
-        #         pass
-        # return True
         stack=[]
         for i in s:
             if i in brac_dict:
